ec0ce481b6789c8d50b6014b86b667e4c10e179d
Zensxu <110100664+Zensxu@users.noreply.github.com>
2022-07-28 19:58:55 +0700

    Add files via upload

---
+++ getproxy.sh
@@ -0,0 +1,3 @@
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt -o proxy.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt -o proxy.txt;
+curl https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/http.txt -o proxy.txt;
