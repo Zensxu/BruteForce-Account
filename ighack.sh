d0b1b8e9bdd84c961f5989c8d8fbd30f1ca312be
Zensxu <110100664+Zensxu@users.noreply.github.com>
2022-07-28 10:39:30 +0700

    Update ighack.sh

--- ighack.sh
+++ ighack.sh
@@ -77,9 +77,9 @@ echo -e '\e[91m
     ╚═╝ ╚═════╝       \e[92m╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝\e[91m
 \e[0m'
 printf "\n"
-printf "                \e[101m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
+printf "                \e[101m\e[1;77m  >>  Script By Type -0 << \e[0m\n"
 printf "\n"
-printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Hackers \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 }
 baner1(){
@@ -104,9 +104,9 @@ echo -e '\e[91m
     ╚═╝ ╚═════╝       \e[92m╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝\e[91m
 \e[0m'
 printf "\n"
-printf "                \e[101m\e[1;77m  >>  Script By N17R0 << \e[0m\n"
+printf "                \e[101m\e[1;77m  >>  Script By Type -0 << \e[0m\n"
 printf "\n"
-printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m Noob Hackers \e[100m\e[1;77m << \e[0m\n"
+printf "       \e[100m\e[1;77m >>  Youtube Channel : \e[1;96m    Hackers \e[100m\e[1;77m << \e[0m\n"
 printf "\n"
 }
 echo 
@@ -238,13 +238,13 @@ echo -e "
       ┃             ┃| $grn$cod UPTIME=$o" $upt"$grn
       ┃  ( ͡❛ ͜ʖ ͡❛)   ┃| $grn$cod SHELL=$o" $shl"$grn
       ┃             ┃| $grn$cod EGL=$o" $egl"$grn
-      ┃ Noob Hackers┃| $grn$cod HARDWARE=$o" $hardw"$grn
+      ┃      Hackers┃| $grn$cod HARDWARE=$o" $hardw"$grn
       ┃━━━━━ ○ ━━━━━┃| $grn$cod MODEL=$o" $modl"$grn
       ╰━━━━━━━━━━━━━╯
 "
 center "^CHECKING DONE^"
 echo -e "\e[93m                     Subscribe On Youtube\e[92m"
-center "<NOOB HACLERS>"
+center "<HACLERS>"
 sleep 20.0
 more
 }
@@ -306,13 +306,13 @@ echo -e '\e[91m
                  ────▌──▐────
 '
 echo -e "\e[93m                  CALL ME \e[92mNITRO" | pv -qL 10
-echo -e "\e[93m             IAM FROM \e[92mNOOB HACKERS" | pv -qL 10
+echo -e "\e[93m             IAM FROM \e[92m    HACKERS" | pv -qL 10
 echo -e "\e[93m       IM A \e[92mGEEK\e[93m WITH LOTS OF EXCITEMENT" | pv -qL 10
 echo -e "\e[93m             HOPE YOU LIKE THIS SCRIPT" | pv -qL 10
 echo -e "\e[93m         OOPS... I TALK A LOT SRY FOR THAT " | pv -qL 10
 echo -e "\e[93m              JOIN GROUPS ON \e[92mWHATS' APP" | pv -qL 10
-echo -e "\e[93m        MY WEBSITE:\e[92m www.noob-hackers.com\e[0m" | pv -qL 10
-echo -e "\e[93m        PRACTICAL VIDEOS:\e[92m www.noobhacktube.com\e[0m" | pv -qL 10
+echo -e "\e[93m        MY WEBSITE:\e[92m www.-hackers.com\e[0m" | pv -qL 10
+echo -e "\e[93m        PRACTICAL VIDEOS:\e[92m www.hacktue.com\e[0m" | pv -qL 10
 echo -e "\e[92m                BYEE.............." | pv -qL 10
 sleep 8.0
 baner1
