a51df90a0c353d998837adcfb77b9c7b31f632f4
Zensxu <110100664+Zensxu@users.noreply.github.com>
2022-07-28 15:29:24 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -12,7 +12,7 @@ if sys.version_info[0] != 3:
     fb.py\n\t--------------------------------------''')
     sys.exit()
 
-PASSWORD_FILE = "passwords.txt"
+PASSWORD_FILE = "pass.txt"
 MIN_PASSWORD_LENGTH = 6
 POST_URL = 'https://www.facebook.com/login.php'
 HEADERS = {
